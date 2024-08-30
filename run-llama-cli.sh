186339f8c7fe4e56db6a45abb845dbf91ca8ef2d
Denis Oliver Kropp <Leichenbegatter@outlook.com>
2024-08-30 15:45:53 +0200

    qllx-v1, QllickBuzz
    $ time llama-cli -m llama3-discoleo-instruct-8b-v0.1-iq4_nl-imat.gguf --temp 0.0 -f qllickbuzz-system2.llama3 -sp -co --keep -1 -f qllickbuzz-marky.llama3  -f README.md -f qgen-assistant.llama3
    
    ....
    
    <|start_header_id|>assistant<|end_header_id|>
    [QllickBuzz | AI Developer] The repository is a valuable resource for anyone interested in exploring the technical, philosophical, and ethical considerations surrounding artificial intelligence and autonomous agents. It provides a comprehensive overview of the repository's contents, including the potential of the "e()" function, the benefits of layered architectures, the possibilities and challenges of multi-persona AI systems, and the ethical considerations surrounding agent

---
+++ run-llama-cli.sh
@@ -0,0 +1,1 @@
+time llama-cli -m llama3-discoleo-instruct-8b-v0.1-iq4_nl-imat.gguf --temp 0.0 -f qllickbuzz-system2.llama3 -sp -co --keep -1 -f qllickbuzz-marky.llama3  -f README.md -f qgen-assistant.llama3
